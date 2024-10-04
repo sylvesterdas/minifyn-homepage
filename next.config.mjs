433362ee66038825302e2a78fb6f5a8520c3b3aa
sylvesterdas <sylvester.das@gmail.com>
2024-10-04 12:07:29 +0530

    config, fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 import i18nConfig from './next-i18next.config.js';
 
+const allowedOrigins = ['https://www.minifyn.com', 'https://www.mnfy.in'];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   i18n: i18nConfig.i18n,
   serverRuntimeConfig: {
-    // Will only be available on the server side
     NEXT_RECAPTCHA_SECRET_KEY: process.env.NEXT_RECAPTCHA_SECRET_KEY,
     NEXT_RAZORPAY_KEY_ID: process.env.NEXT_RAZORPAY_KEY_ID,
     NEXT_RAZORPAY_KEY_SECRET: process.env.NEXT_RAZORPAY_KEY_SECRET,
   },
   publicRuntimeConfig: {
-    // Will be available on both server and client
     BASE_URL: process.env.BASE_URL,
     NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
   },
@@ -19,17 +19,45 @@ const nextConfig = {
     return [
       {
         source: '/',
-        has: [
-          {
-            type: 'host',
-            value: 'www.mnfy.in',
-          },
-        ],
+        has: [{ type: 'host', value: 'www.mnfy.in' }],
         destination: 'https://www.minifyn.com',
         permanent: false,
       },
+      {
+        source: '/dashboard',
+        has: [{ type: 'host', value: 'www.mnfy.in' }],
+        destination: 'https://www.minifyn.com/dashboard',
+        permanent: false,
+      },
+      {
+        source: '/dashboard/:path*',
+        has: [{ type: 'host', value: 'www.mnfy.in' }],
+        destination: 'https://www.minifyn.com/dashboard/:path*',
+        permanent: false,
+      },
     ];
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: allowedOrigins.join(','),
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
