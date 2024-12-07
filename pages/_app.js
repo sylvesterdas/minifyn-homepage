0a169738f70a131a1c39dbf3810cf30898be70fa
Sylvester Das <sylvester.das@gmail.com>
2024-12-07 15:17:13 +0530

    osano banner

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import { appWithTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
-import PublicLayout from '../components/PublicLayout';
-import DashboardLayout from '../components/dashboard/DashboardLayout';
+import PublicLayout from '@/components/PublicLayout';
+import DashboardLayout from '@/components/dashboard/DashboardLayout';
 import { AuthProvider } from '@/contexts/AuthContext';
+import ConsentManager from '@/components/ConsentManager';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
@@ -11,20 +12,21 @@ function MyApp({ Component, pageProps }) {
   const isDashboardRoute = router.pathname.startsWith('/dashboard');
   const Layout = isDashboardRoute ? DashboardLayout : PublicLayout;
 
+  const body = (
+      <Layout>
+        <ConsentManager />
+        <Component {...pageProps} />
+      </Layout>
+  )
+
   if (router.route !== '/[shortCode]')
     return (
       <AuthProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        {body}
       </AuthProvider>
     );
   else {
-    return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    );
+    return body;
   }
 }
 
