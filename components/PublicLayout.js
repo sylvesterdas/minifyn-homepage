b212a3ed8ef43bb2860d0bfebfd7e2e51519ac5f
Sylvester Das <sylvester.das@gmail.com>
2024-12-08 17:48:41 +0530

    feat: initialize GA4 once

--- components/PublicLayout.js
+++ components/PublicLayout.js
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './navigation/Navbar';
 import Footer from './Footer';
 import { useRouter } from 'next/router';
@@ -8,19 +8,26 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 
 const PublicLayout = ({ children }) => {
   const router = useRouter();
+  // Only initialize GA once
+  const [gaInitialized] = useState(() => (
+    <GoogleAnalytics 
+      gaId={process.env.NEXT_PUBLIC_GA_ID} 
+      debugMode={process.env.NODE_ENV === 'development'} 
+    />
+  ));
 
   const body = (
     <main className="flex-grow">
-      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} debugMode={process.env.NODE_ENV === 'development'} />
+      {gaInitialized}
       {children}
     </main>
-  )
+  );
 
   if (router.route !== '/[shortCode]') {
     return (
       <div className="flex flex-col min-h-screen bg-gradient-to-b from-blue-50 to-white">
         <Navbar />
-          {body}
+        {body}
         <Footer />
       </div>
     );
