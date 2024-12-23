e1ce8c5289e3324c03ca2cc922a04b848361b1ba
sylvesterdas <sylvester.das@gmail.com>
2024-12-23 20:46:29 +0530

    chore: clean up

--- pages/dashboard/analytics.js
+++ pages/dashboard/analytics.js
@@ -1,25 +1,19 @@
 import Head from 'next/head';
 import ComingSoon from '@/components/dashboard/ComingSoon';
-import dynamic from 'next/dynamic';
 
-const Analytics = dynamic(
-  () => {
-    return (
-      <>
-        <Head>
-          <title>Analytics | MiniFyn Dashboard</title>
-        </Head>
+const Analytics = () => (
+  <>
+    <Head>
+      <title>Analytics | MiniFyn Dashboard</title>
+    </Head>
 
-        <h1 className="text-2xl font-semibold text-gray-900 mb-6">Analytics</h1>
-        
-        <ComingSoon 
-          title="Advanced Analytics Dashboard"
-          description="Detailed insights into your links' performance, including geographic data, device breakdowns, and custom reports are on the way!"
-        />
-      </>
-    );
-  },
-  { ssr: false }
+    <h1 className="text-2xl font-semibold text-gray-900 mb-6">Analytics</h1>
+    
+    <ComingSoon 
+      title="Advanced Analytics Dashboard"
+      description="Detailed insights into your links' performance, including geographic data, device breakdowns, and custom reports are on the way!"
+    />
+  </>
 );
 
 export default Analytics
