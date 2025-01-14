e051c5c6da27ee10d114b32648bf9458da607851
sylvesterdas <sylvester.das@gmail.com>
2025-01-14 13:05:15 +0000

    chore: fresh project

--- components/navigation/navConfig.js
+++
@@ -1,7 +0,0 @@
-export const NAV_ITEMS = [
-  { key: 'Home', path: '/' },
-  { key: 'Features', path: '/features' },
-  { key: 'Blog', path: '/blog' },
-  { key: 'Pricing', path: '/pricing' },
-  { key: 'API Docs', path: '/api-docs' }
-];
