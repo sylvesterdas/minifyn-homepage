76c073a88adc9fcc195fa59a8a80971a0d05546e
sylvesterdas <sylvester.das@gmail.com>
2025-02-24 00:02:39 +0530

    fix: cleanup

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,12 +1,9 @@
-import { Sidebar } from '@/components/sidebar'
-
 export default function BlogLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return <div className="flex flex-row flex-1">
-      {/* <Sidebar className={"max-md:hidden"} onPress={undefined} /> */}
       <main className="w-full">{children}</main>
     </div>
 }
